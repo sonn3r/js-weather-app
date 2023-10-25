7c837a66a28acbd72f532392923ea99b765cccd0
Andrii Sakhno <andysakhno@gmail.com>
2023-10-25 23:47:15 +0300

    New API

--- main.js
+++ main.js
@@ -12,7 +12,21 @@ search.addEventListener('click', () => {
         return;
     }
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(response => response.json()).then(json => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(json => {
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
@@ -22,44 +36,42 @@ search.addEventListener('click', () => {
         switch (json.weather[0].main) {
             case 'Clear':
                 image.src = 'images/clear.png';
+                break;
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+            case 'Cloud':
+                image.src = 'images/cloud.png';
+                break;
+            case 'Haze':
+                image.src = 'images/mist.png';
+                break;
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+            default:
+                image.src = '';
         }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
     });
-
-
 });
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const searchButton = document.getElementById('searchButton');
-    const cityInput = document.getElementById('cityInput');
-    const weatherResult = document.getElementById('weatherResult');
-
-    searchButton.addEventListener('click', function () {
-        const city = cityInput.value;
-
-        if (city.trim() !== '') {
-            // Replace 'YOUR_API_KEY' with your OpenWeather API key
-            const apiKey = '5eabeb55f152b01a0367fc3e48a65afc';
-            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    weatherResult.innerHTML = `
-            <h2>Weather in ${data.name}, ${data.sys.country}</h2>
-            <p>Temperature: ${data.main.temp}°C</p>
-            <p>Weather: ${data.weather[0].description}</p>
-            <p>Humidity: ${data.main.humidity}%</p>
-            <p>${data.main.clouds}</p>
-          `;
-                    weatherResult.style.display = 'block'; // Show the weather information div
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                    weatherResult.innerHTML = 'Error fetching weather data.';
-                    weatherResult.style.display = 'block'; // Show the error message
-                });
-        }
-    });
+const searchInput = document.querySelector('.search-box input');
+searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        // Prevent the default form submission
+        event.preventDefault();
+        // Trigger the search when Enter is pressed
+        search.click();
+    }
 });
